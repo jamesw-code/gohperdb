047daa5d068c894312c4d22b286223e7b53c208e
James Wyatt <jamesrwyatt35@gmail.com>
2025-05-31 19:51:15 -0400

    expand case

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"gopherdb/store"
 	"net"
 	"strings"
 )
@@ -47,13 +48,47 @@ func handleConnection(conn net.Conn) {
 
 		switch strings.ToUpper(parts[0]) {
 		case "SET":
+			if len(parts) != 3 {
+				conn.Write([]byte("ERR: usage SET key value\n"))
+				return
+			}
+			store.Set(parts[1], parts[2])
+			conn.Write([]byte("OK\n"))
 
 		case "GET":
+			if len(parts) != 2 {
+				conn.Write([]byte("ERR: usage GET key\n"))
+				return
+			}
+			val, ok := store.Get(parts[1])
+			if !ok {
+				conn.Write([]byte("NULL\n"))
+			} else {
+				conn.Write([]byte(val + "\n"))
+			}
 
 		case "DELETE":
+			if len(parts) != 2 {
+				conn.Write([]byte("ERR: usage DELETE key\n"))
+				return
+			}
+			store.Delete(parts[1])
+			conn.Write([]byte("OK\n"))
+
+		case "EXISTS":
+			if len(parts) != 2 {
+				conn.Write([]byte("ERR: usage EXISTS key\n"))
+				return
+			}
+			_, ok := store.Get(parts[1])
+			if ok {
+				conn.Write([]byte("1\n"))
+			} else {
+				conn.Write([]byte("0\n"))
+			}
 
 		case "PING":
-			_, _ = conn.Write([]byte("PONG\n"))
+			conn.Write([]byte("PONG\n"))
 		default:
 			_, _ = conn.Write([]byte("ERR: unknown command\n"))
 		}
